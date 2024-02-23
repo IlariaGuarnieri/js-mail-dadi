8a1cf4cd5bb313ca8aecf621384dc92762dfa437
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-02-23 18:00:19 +0100

    [edit] prove variabili

--- README.md
+++ README.md
@@ -5,9 +5,26 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 
-# sviluppo
+# sviluppo (ilaria version)
 1. prompt chiedi all'utente la mail
 2. controllare che l'email sia valida e acccettabile con array:
 creo arrey in cui all'interno scrivo una serie di mail (mail della lista di chi puo accedere)
 4. apro delle variabili scope in cui verificare se la mail inserita dall'utente corrisponde a quelle della lista
 3. generare un messaggio con l'esito
+
+
+# sviluppo (paolo version)
+Chiedi all’utente la sua email:
+-a chiedere insermento mail a utente (prompt o form)
+-b memorizzo il valore per usarlo succesivamente (varibile)
+
+controlla che sia nella lista di chi può accedere,
+
+ -c crea una lista di mail (array) --> variabile?
+ -d controllo ad uno a uno gli elementi della lista (for,array)
+ -e quando la mail dell'utente è uguale ad una delle mail che ho nella lista --> salvo da qualche parte (varibaile) che l'ho trovata
+
+stampa un messaggio appropriato sull’esito del controllo.
+
+-f se l'ho trovata stampo che è .... if()
+-g se non l'ho trovato stampo che non è... else
--- script.js
+++ script.js
@@ -1,32 +1,49 @@
-const containerMail= document.getElementById('container-a');
 
 let mailUtente = prompt('Inserisci qui la tua mail');
 console.log('mailUtente');
 
-const mailAccettate = document.getElementById('mail-Accettate');
-
 // array con mail in lista
-
-let mailAccetate =['ciao@gmail.com' ,'ugo@gmail.com' , 'mario@gmail.com']
-console.log(mail);
+let mailAccetate =['marco@gmail.com' ,'ugo@gmail.com' , 'mario@gmail.com']
+console.log(mailAccetate);
+console.log(mailAccetate[0]);
+console.log(mailAccetate[1]);
+console.log(mailAccetate[2]);
 
 // variabili scope per verificare se utente è in lista
-if (mailUtente === mailAccettate){ 
+if (mailUtente === mailAccettate[0]){ 
   stampa = 'benvenuto';
 } 
 else {
   stampa= 'non sei nella lista';
 }
 
+if (mailUtente === mailAccettate[1]){ 
+  stampa = 'benvenuto';
+} 
+else {
+  stampa= 'non sei nella lista';
+}
+
+if (mailUtente === mailAccettate[2]){ 
+  stampa = 'benvenuto';
+} 
+else {
+  stampa= 'non sei nella lista';
+}
+
+
 document.querySelector('container-a').innerHTML +=`
-<div class="container ${}
+<div class="container ${stampa}> ${stampa} </div>
 `
 
-// containerMail.innerHTML += `
-// <div class="container ${stampa}">${stampa}</div>
-// `
+// let mailAccetate = document.getElementById('mail-Accettate');
+// for(let i = 0; i < 4; i++){
+//   if(mailUtente === mailAccettate[ ]){ 
+//     console.log(i)
+//     stampa = 'benvenuto';
+//     console.log(mailAccetate)
+//   }
+// }
 
-// let mailAccetate = document.getElementById ('mail')
 
-// let mailUtente = prompt('inserisci la tua mail');
-// console.log
+
